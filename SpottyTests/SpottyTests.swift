28ddf9d853cea572ec64e37e1cf784ff24b227da
Gennaro Frazzingaro <frazzingaro@apple.com>
2018-12-03 13:51:37 +0100

    Initial Commit

---
+++ SpottyTests/SpottyTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpottyTests.swift
+//  SpottyTests
+//
+//  Created by Gennaro on 12/3/18.
+//  Copyright © 2018 Gennaro. All rights reserved.
+//
+
+import XCTest
+@testable import Spotty
+
+class SpottyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
